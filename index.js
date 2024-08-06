d5cd31547e8f949455976194514e912d3df6e35a
Purnama S Rahayu <purnamasrahayu@gmail.com>
2024-08-06 10:24:18 +0700

    layout success message

--- index.js
+++ index.js
@@ -1,10 +1,17 @@
 const form = document.getElementById("form-newsletter");
 const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
 
-const errorMessageElement = document.querySelector(".form__error-message")
+const errorMessageElement = document.querySelector(".form__error-message");
 const successSection = document.querySelector(".section--success");
 const initialSection = document.querySelector(".section--initial");
 
+const formInput = document.querySelector(".form__input");
+
+function validateEmail(email) {
+    const isValid = emailRegex.test(email);
+
+    return isValid
+}
 
 function handleSubmit(e) {
   e.preventDefault();
@@ -12,23 +19,39 @@ function handleSubmit(e) {
   const formData = new FormData(e.target);
   const { email } = Object.fromEntries(formData);
 
-  isValidEmail = emailRegex.test(email)
-  console.log(isValidEmail, errorMessageElement)
+  const isValidEmail = validateEmail(email)
 
   if (!isValidEmail) {
+    // Show the error message
     errorMessageElement.classList.remove("hidden");
-    errorMessageElement.classList.add("show")
+    errorMessageElement.classList.add("show");
+
+    // Reset the animation
+    errorMessageElement.classList.remove("form__error-message--invalid");
+    void errorMessageElement.offsetWidth; // Trigger reflow to restart animation
+    errorMessageElement.classList.add("form__error-message--invalid");
   } else {
-    initialSection.classList.add("hidden")
-    successSection.classList.remove("hidden")
-    successSection.classList.add("show")
+    initialSection.classList.add("hidden");
+    successSection.classList.remove("hidden");
+    successSection.classList.add("show");
   }
 }
 
 form.addEventListener("submit", handleSubmit);
 
+formInput.addEventListener("input", function(e) {
+    const isValidEmail = validateEmail(e.target.value);
+
+    if (isValidEmail) {
+        console.log('vaild!');
+        errorMessageElement.classList.remove("show")
+        errorMessageElement.classList.add("hidden")
+        errorMessageElement.classList.remove("form__error-message--invalid")
+    }
+})
+
 const dismissButton = document.querySelector(".section--success__main__button");
 
-dismissButton.addEventListener('click', function() {
-    window.location.reload()
-})
+dismissButton.addEventListener("click", function () {
+  window.location.reload();
+});
